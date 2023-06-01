7834d8625ea106c59ebf13ea33ab3307a1cff0f4
wangmou23 <108864901+wangmou23@users.noreply.github.com>
2023-06-01 15:53:42 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,6 +10,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("1");
         System.out.println("push commit");
-
+        System.out.println("pull commit");
     }
 }
