da15281c1eb683e1c5bbbb0b030d013d151e83e2
Wangmou <1528297511@qq.com>
2023-06-01 14:35:13 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hellow git!");
         System.out.println("hellow git!2");
+        System.out.println("hellow git!2");
     }
 }
